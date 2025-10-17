79bba0267423815723c9f589ca6029cf5f4e39bd
重装小兔19 <101702244+zyr3210115527@users.noreply.github.com>
2025-10-17 10:38:26 +0800

    Add files via upload

--- heap_sort.py
+++ heap_sort.py
@@ -22,4 +22,26 @@ def heapsort(nums):
         #调整二叉树大小，最大的剔除了已经归位的数
     return nums
 
+def heapy_reverse(arr,n,i):
+    min=i
+    left=2*i+1
+    right=2*i+2
+    if left<n and arr[left]<arr[min]:
+        min=left
+    if right<n and arr[right]<arr[min]:
+        min=right
+    if min!=i:
+        arr[i],arr[min]=arr[min],arr[i]
+        heapy_reverse(arr,n,min)
+
+def heapsort_reverse(nums):
+    n=len(nums)
+    for i in range(n//2-1,-1,-1):
+        heapy_reverse(nums,n,i)
+    for i in range(n-1,0,-1):
+        nums[i],nums[0]=nums[0],nums[i]
+        heapy_reverse(nums,i,0)
+    return nums
+
 print(heapsort([1,4,2,5,3]))
+print(heapsort_reverse([1,4,2,5,3]))
